2a5cd6cb86af9d6ba4ae717a57daadb03040e82b
sora <sora@haeena.net>
2017-01-27 06:05:20 -0800

    update

--- ethsim.c
+++ ethsim.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <time.h>
 
+#include <sys/param.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -14,8 +15,6 @@
 #include <netinet/in.h>
 #include <netinet/ip6.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <net/route.h>
 
 #include <linux/if.h>
 #include <linux/if_tun.h>
@@ -24,19 +23,32 @@
 
 #define MAXNUMDEV    8
 
-#define TAP_PATH     "/dev/net"
+#define TAP_PATH     "/tmp"
 #define TAP_NAME     "phy"
 #define TAP_MAJOR    10
 #define TAP_MINOR    200
 
+struct tap {
+	char path[MAXPATHLEN];
 
-int tap_mkchardev(char *dev)
+	char dev[IFNAMSIZ];
+
+	int mode;
+	
+	int ndev;
+
+	uid_t uid;
+
+	gid_t gid;
+};
+
+int tap_mkchardev(struct tap *tap)
 {
-	char devpath[IFNAMSIZ + 9];
+	char devpath[IFNAMSIZ + MAXPATHLEN];
 	dev_t devnum = 0;
 	int ret;
 
-	sprintf(devpath, "%s/%s", TAP_PATH, dev);
+	sprintf(devpath, "%s/%s", tap->path, tap->dev);
 
 	devnum = makedev(TAP_MAJOR, TAP_MINOR);
 
@@ -46,19 +58,25 @@ int tap_mkchardev(char *dev)
 		return -1;
 	}
 
+	ret = chown(devpath, tap->uid, tap->gid);
+	if (ret < 0) {
+		perror("chown");
+		return -1;
+	}
+
 	return 0;
 }
 
 /*
  * tap_init
  */
-int tap_init(char *dev, int mode)
+int tap_init(struct tap *tap)
 {
-	char devpath[IFNAMSIZ + 9];
+	char devpath[IFNAMSIZ + MAXPATHLEN];
 	struct ifreq ifr;
 	int fd, err;
 
-	sprintf(devpath, "%s/%s", TAP_PATH, dev);
+	sprintf(devpath, "%s/%s", tap->path, tap->dev);
 
 	fd = open(devpath, O_RDWR);
 	if (fd < 0) {
@@ -68,8 +86,8 @@ int tap_init(char *dev, int mode)
 
 	memset(&ifr, 0, sizeof(ifr));
 	ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
-	if (*dev) {
-		strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+	if (tap->dev) {
+		strncpy(ifr.ifr_name, tap->dev, IFNAMSIZ);
 	}
 
 	err = ioctl(fd, TUNSETIFF, (void *)&ifr);
@@ -79,15 +97,29 @@ int tap_init(char *dev, int mode)
 		return err;
 	}
 
+	err = ioctl(fd, TUNSETOWNER, tap->uid);
+	if (err < 0) {
+		perror("TUNSETOWNER");
+		close(fd);
+		return err;
+	}
+
+	err = ioctl(fd, TUNSETGROUP, tap->gid);
+	if (err < 0) {
+		perror("TUNESETGROUP");
+		close(fd);
+		return err;
+	}
+
 	/* enable persistent mode */
-	if (ioctl(fd, TUNSETPERSIST, mode) != 0) {
+	if (ioctl(fd, TUNSETPERSIST, tap->mode) != 0) {
 		perror("ioctl(TUNSETPERSIST)");
 	}
 
 	return fd;
 }
 
-int tap_config(char *dev, int n)
+int tap_config(struct tap *tap, int n)
 {
 	struct sockaddr_in *s_in;
 	struct ifreq ifr;
@@ -98,7 +130,7 @@ int tap_config(char *dev, int n)
 		return -1;
 	}
 
-	strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+	strncpy(ifr.ifr_name, tap->dev, IFNAMSIZ);
 
 	if (ioctl(fd, SIOCGIFFLAGS, &ifr) != 0) {
 		perror("ioctl(SIOCGIFFLAGS)");
@@ -135,11 +167,11 @@ int tap_config(char *dev, int n)
 	return 0;
 }
 
-int tap_release(char *dev)
+int tap_release(struct tap *tap)
 {
-	char devpath[IFNAMSIZ + 9];
+	char devpath[IFNAMSIZ + MAXPATHLEN];
 
-	sprintf(devpath, "%s/%s", TAP_PATH, dev);
+	sprintf(devpath, "%s/%s", tap->path, tap->dev);
 	unlink(devpath);
 
 	return 0;
@@ -147,7 +179,7 @@ int tap_release(char *dev)
 
 void usage(void)
 {
-	fprintf(stderr, "Usage:tapdev {add or del} {number_of_devices}\n");
+	fprintf(stderr, "Usage:tapdev {add or del} {number_of_devices} {uid} {groupid}\n");
 }
 
 
@@ -156,61 +188,75 @@ void usage(void)
  */
 int main(int argc, char **argv)
 {
-	char dev[IFNAMSIZ];
-	int mode, i, ndev, ret;
 	char mode_ascii[4];
+	struct tap tap;
+	int i, ret;
 
-	if (argc != 3) {
+	if (argc != 5) {
 		usage();
 		return 1;
 	}
 
 	strncpy(mode_ascii, argv[1], 4);
 	if ((ret = strcmp(mode_ascii, "add")) == 0) {
-		mode = 1;
+		tap.mode = 1;
 	} else if ((strcmp(mode_ascii, "del")) == 0) {
-		mode = 0;
+		tap.mode = 0;
 	} else {
 		printf("unknown command: %s\n", mode_ascii);
 		return 1;
 	}
 
-	ndev = atoi(argv[2]);
-	if (ndev < 1 || ndev > MAXNUMDEV) {
+	tap.ndev = atoi(argv[2]);
+	if (tap.ndev < 1 || tap.ndev > MAXNUMDEV) {
 		usage();
 		return 1;
 	}
 
-	/* tap device setup */
-	for (i = 0; i < ndev; i++) {
-		sprintf(dev, "%s%d", TAP_NAME, i);
+	tap.uid = atoi(argv[3]);
+	if (tap.uid < 0) {
+		usage();
+		return 1;
+	}
 
-		if (mode == 1) {
-			ret = tap_mkchardev(dev);
+	tap.gid = atoi(argv[4]);
+	if (tap.gid < 0) {
+		usage();
+		return 1;
+	}
+
+	strncpy(tap.path, TAP_PATH, sizeof(tap.path));
+
+	/* tap device setup */
+	for (i = 0; i < tap.ndev; i++) {
+		sprintf(tap.dev, "%s%d", TAP_NAME, i);
+
+		if (tap.mode == 1) {
+			ret = tap_mkchardev(&tap);
 			if (ret < 0) {
 				perror("tap_mkchardev");
 				return 1;
 			}
 
-			ret = tap_init(dev, mode);
+			ret = tap_init(&tap);
 			if (ret < 0) {
 				perror("tap_init");
 				return 1;
 			}
 
-			ret = tap_config(dev, i);
+			ret = tap_config(&tap, i);
 			if (ret < 0) {
 				perror("tap_config");
 				return 1;
 			}
-		} else if (mode == 0) {
-			ret = tap_init(dev, mode);
+		} else if (tap.mode == 0) {
+			ret = tap_init(&tap);
 			if (ret < 0) {
 				perror("tap_init");
 				return 1;
 			}
 
-			ret = tap_release(dev);
+			ret = tap_release(&tap);
 			if (ret < 0) {
 				perror("tap_release");
 				return 1;
